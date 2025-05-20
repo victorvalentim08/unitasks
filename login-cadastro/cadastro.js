7a9ce23224949b2656ba3d096a5ad68be01f3696
misaelcsc <misa02022004@gmail.com>
2025-05-20 10:42:27 -0300

    AGORA DEU CERTO

---
+++ login-cadastro/cadastro.js
@@ -0,0 +1,6 @@
+document.getElementById("formulario").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const nome = document.querySelector('input[name="nome"]').value.trim();
+    
+})
