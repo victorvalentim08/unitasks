2f1c9a4d1e14b68d496bc180e14fe5529093da17
misaelcsc <misa02022004@gmail.com>
2025-05-24 16:51:06 -0300

    tomara que tenha dado certo

--- login-cadastro/cadastro.js
+++ login-cadastro/cadastro.js
@@ -1,6 +1,33 @@
-document.getElementById("formulario").addEventListener("submit", function(event){
+console.log('js carregado');
+
+document.querySelector(".formulario").addEventListener("submit", function(event){
     event.preventDefault();
 
     const nome = document.querySelector('input[name="nome"]').value.trim();
-    
-})
+    const matricula = document.querySelector('input[name="matricula"]').value.trim();
+    const curso = document.getElementById("curso").value;
+    const polo = document.getElementById("polo").value;
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+    const termos = document.getElementById("termos").checked;
+
+    if(!nome || !matricula || !curso || !polo || !email || !senha){
+        alert("Por favor, preencha todos os campos");
+        return
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Digite um e-mail válido.");
+        return;
+    }
+
+     if (!termos) {
+        alert("Você precisa aceitar os termos de uso.");
+        return;
+    }
+
+    alert("Cadastro realizado com sucesso!");
+
+    window.location.href = "/index.html";
+});
