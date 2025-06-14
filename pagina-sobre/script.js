cbe342b37e9a6366720534cc2be3697aa4691b18
yuri-andrade1 <yuridelimaandrade34@gmail.com>
2025-06-13 21:29:59 -0300

    ajeitando chuva

--- pagina-sobre/script.js
+++ pagina-sobre/script.js
@@ -1,11 +1,13 @@
-const chuva = document.querySelector('.chuva');
-chuva.innerHTML = '';
+const chuva = document.getElementById('chuva');
+if (chuva) {
+  chuva.innerHTML = '';
 
-for (let i = 0; i < 150; i++) {
-  const drop = document.createElement('div');
-  drop.classList.add('drop');
-  drop.style.left = `${Math.random() * 100}vw`;
-  drop.style.animationDuration = `${0.5 + Math.random() * 1.5}s`;
-  drop.style.animationDelay = `${Math.random() * 5}s`;
-  chuva.appendChild(drop);
+  for (let i = 0; i < 150; i++) {
+    const drop = document.createElement('div');
+    drop.classList.add('drop');
+    drop.style.left = `${Math.random() * 100}vw`;
+    drop.style.animationDuration = `${0.5 + Math.random() * 1.5}s`;
+    drop.style.animationDelay = `${Math.random() * 5}s`;
+    chuva.appendChild(drop);
+  }
 }
